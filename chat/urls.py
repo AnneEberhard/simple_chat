c56bed2fe0188dfe1bffe47fa254df41dddd87c8
AnneEberhard <anne.eberhard@gmx.net>
2024-06-04 17:21:42 +0200

    updated urls

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from . import views
 # URL patterns for the chat application. These routes handle viewing the chat index
 # and individual chat rooms identified by their slugs.
 urlpatterns = [
-   # path("", views.chat, name="chat"),  # Root path for the chat index view.
+    path("", views.index, name="index"),  # Root path for the chat index view.
     path('<str:room_slug>/', views.room, name="room"),  # Path for individual chat rooms.
     path('send-message/<str:room_slug>/', views.send_message, name="send_message"),
 ]
